a5438db329ced0d44bdecf575d59654f5f5fe81d
Michal Kral <michal.kral@startupjobs.cz>
2025-12-05 18:32:28 +0100

    Refactor code style for consistency and clarity in various files

--- src/Transport/Config/ConfigHelper.php
+++ src/Transport/Config/ConfigHelper.php
@@ -64,7 +64,7 @@ class ConfigHelper
     {
         /** @var array<string, mixed>|null $filteredValue */
         $filteredValue = self::getType($config, $key, self::TYPE_ARRAY);
-        assert($filteredValue === null);
+        assert(is_array($filteredValue) || $filteredValue === null);
 
         return $filteredValue;
     }
